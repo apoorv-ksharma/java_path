b13fcb353797c9df5fb10c9f75014be0a0f7a075
Apoorv Sharma <apoorv.kumar.sharma@gmail.com>
2025-07-15 15:06:40 +0530

    chore: static

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
-    public static void main(String [] args){
-        System.out.print("Hello World");
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Test.increment();
+        System.out.println(Test.number);
+        Test.increment();
+
+        System.out.println(Test.number);
+
+    }
+}
+
+class Test {
+    static {
+        System.out.println("class initialized");
+    }
+
+    public static int number = 0;
+
+    public static void increment() {
+        number++;
     }
 }
